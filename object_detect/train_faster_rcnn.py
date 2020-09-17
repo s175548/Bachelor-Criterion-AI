f2221432ac5f12280efe2f43f945b78fa799b6a5
s175549 <s175549@student.dtu.dk>
2020-09-17 08:27:04 +0200

    ASd

--- object_detect/train_faster_rcnn.py
+++ object_detect/train_faster_rcnn.py
@@ -2,40 +2,43 @@ from tqdm import tqdm
 import torchvision
 import numpy as np
 from torchvision import datasets, transforms, utils
+import object_detect.utils_p as utils_p
 import argparse
 import os
 from torch.utils import data
 from PIL import Image
 import torch
-from torchvision.models.vgg import vgg16
-from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from semantic_segmentation.DeepLabV3.utils import ext_transforms as et
+#from object_detect.coco_eval import CocoEvaluator
 
-def initialize_model(num_classes,backbone,out_channels):
-    anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
-    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=[0], output_size=7, sampling_ratio=2)
-    #backbone = vgg16(pretrained=True).features
-    backbone.out_channels = out_channels
-    model = FasterRCNN(backbone,
-                       num_classes=num_classes,
-                       rpn_anchor_generator=anchor_generator,
-                       box_roi_pool=roi_pooler)
-    for param in model.parameters():
-        param.requires_grad = False
-    return model
+def get_coco_api_from_dataset(dataset):
+    for i in range(10):
+        if isinstance(dataset, torchvision.datasets.CocoDetection):
+            break
+        if isinstance(dataset, torch.utils.data.Subset):
+            dataset = dataset.dataset
+    if isinstance(dataset, torchvision.datasets.CocoDetection):
+        return dataset.coco
+    return convert_to_coco_api(dataset)
 
 def init_model(num_classes):
-    #load a model pre-trained pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    # replace the classifier with a new one, that has
-    # num_classes which is user-defined
-    num_classes = num_classes  # 1 class (person) + background
-    # get number of input features for the classifier
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get the number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
     return model
 
 def get_argparser():
@@ -120,10 +123,82 @@ def validate(model, loader, device, metrics, ret_samples_ids=None):
         print(score)
     return score, ret_samples
 
-transform_function = et.ExtCompose([et.ExtTransformLabel(),et.ExtCenterCrop(512),et.ExtScale(300),
+transform_function = et.ExtCompose([et.ExtTransformLabel(),et.ExtCenterCrop(512),et.ExtScale(100),
                 et.ExtToTensor(),
                 et.ExtNormalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225]),])
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
+    model.train()
+    for images, labels in (data_loader):
+        images = list(img.to(device, dtype=torch.float32) for img in images)
+        labels = labels.to(device, dtype=torch.long)        #t
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = utils_p.reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        loss_value = losses_reduced.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+def evaluate(model, data_loader, device):
+    n_threads = torch.get_num_threads()
+    # FIXME remove this and make paste_masks_in_image run on the GPU
+    torch.set_num_threads(1)
+    cpu_device = torch.device("cpu")
+    model.eval()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    coco = get_coco_api_from_dataset(data_loader.dataset)
+    iou_types = _get_iou_types(model)
+    coco_evaluator = CocoEvaluator(coco, iou_types)
+
+    for image, targets in metric_logger.log_every(data_loader, 100, header):
+        image = list(img.to(device) for img in image)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        torch.cuda.synchronize()
+        model_time = time.time()
+        outputs = model(image)
+
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        model_time = time.time() - model_time
+
+        res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
+        evaluator_time = time.time()
+        coco_evaluator.update(res)
+        evaluator_time = time.time() - evaluator_time
+        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    coco_evaluator.synchronize_between_processes()
+
+    # accumulate predictions from all images
+    coco_evaluator.accumulate()
+    coco_evaluator.summarize()
+    torch.set_num_threads(n_threads)
+    return coco_evaluator
 
 class LeatherData(data.Dataset):
 
@@ -163,77 +238,48 @@ class LeatherData(data.Dataset):
 if __name__ == '__main__':
 
     device = torch.device('cpu')
+    #device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print("Device: %s" % device)
 
-    output_stride = 16
-    save_val_results = False
-    total_itrs = 10
-    lr = 0.01
-    lr_policy = 'step'
-    step_size = 10000
-    batch_size = 16
-    val_batch_size = 4
-    loss_type = "cross_entropy"
-    weight_decay = 1e-4
-    random_seed = 1
-    print_interval = 10
-    val_interval = 100
-    vis_num_samples = 2
-    enable_vis = True
+    model = init_model(num_classes=2)
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+
+    # and a learning rate scheduler which decreases the learning rate by
+    # 10x every 3 epochs
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    num_epoch = 1
+    print_freq = 10
 
     train_path_mask = r'C:\Users\johan\OneDrive\Skrivebord\leather_patches\Train_mask'
     test_path_mask = r'C:\Users\johan\OneDrive\Skrivebord\leather_patches\Test_mask'
     train_path_img = r'C:\Users\johan\OneDrive\Skrivebord\leather_patches\Train_img'
     test_path_img = r'C:\Users\johan\OneDrive\Skrivebord\leather_patches\Test_img'
 
-    #model = initialize_model(num_classes=21,backbone=vgg16(pretrained=True).features,out_channels=512)
-    model = init_model(num_classes=2)
-    model.to(device)
-    model.eval()
+    batch_size = 16
+    val_batch_size = 4
 
-    train_dst = LeatherData(path_mask=test_path_mask,path_img=test_path_img,transform=transform_function)
-    val_dst = LeatherData(path_mask=train_path_mask,path_img=train_path_img,transform=transform_function)
+    # Define dataloaders
+    train_dst = LeatherData(path_mask=train_path_mask,path_img=train_path_img,transform=transform_function)
+    val_dst = LeatherData(path_mask=test_path_mask,path_img=test_path_img,transform=transform_function)
+
     train_loader = data.DataLoader(
-       train_dst, batch_size=2, shuffle=True, num_workers=2)
+       train_dst, batch_size=batch_size, shuffle=True, num_workers=2)
     val_loader = data.DataLoader(
         val_dst, batch_size=val_batch_size, shuffle=True, num_workers=2)
     print("Train set: %d, Val set: %d" % (len(train_dst), len(val_dst)))
-    i = 0
 
-    path_test = r'C:/Users/johan/iCloudDrive/DTU/KID/BA/Kode/brevetti/'
-    im = Image.open(path_test+'PennFudanPed/PNGImages/FudanPed00001.png').convert("RGB")
-    im2 = Image.open(path_test+'PennFudanPed/PNGImages/FudanPed00002.png').convert("RGB")
-    img = np.array(im)
-    img2 = np.array(im2)
-    image_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0, 0, 0], [1, 1, 1]),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    img = image_transform(img)
-    img2 = image_transform(img2)
-    imgs = [img, img2]
-    #metric_logger = utils.MetricLogger(delimiter="  ")
-    #header = 'Test:'
-    for (images, labels) in train_loader:
-        if i < 10:
-            images = images.to(device, dtype=torch.float32)
-            labels = labels.to(device, dtype=torch.long)
-            image = list(img.to(device) for img in images)
-            pred = model(images)
-            pr = model(imgs)
-            break
-        else:
-            pass
-
-
-#            a = Image.fromarray(images[0].mul(255).permute(1, 2, 0).byte().numpy())
-#            Image.fromarray(pred[0]['boxes'].mul(255).byte().cpu().numpy())
-#path_test = r'C:/Users/johan/iCloudDrive/DTU/KID/BA/Kode/brevetti/'
-#    img = Image.open(path_test+'PennFudanPed/PNGImages/FudanPed00001.png').convert("RGB")
-#    img2 = Image.open(path_test+'PennFudanPed/PNGImages/FudanPed00002.png').convert("RGB")
-##    img = np.array(img)
- #   img2 = np.array(img2)
- #   image_transform = transforms.Compose([
- #       transforms.ToTensor(), transforms.Normalize([0, 0, 0], [1, 1, 1]),
- #       transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
- ##   image = image_transform(img)
-  #  image2 = image_transform(img2)
-  #  images = [image, image2]
+    for epoch in range(num_epoch):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=print_freq)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        #evaluate(model, data_loader_test, device=device)
