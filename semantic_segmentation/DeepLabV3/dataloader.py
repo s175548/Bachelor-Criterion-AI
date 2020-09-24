97259f8b61650b6bf39b2b6c78af96a38aa91971
Mads C <S173934@student.dtu.dk>
2020-09-24 13:20:30 +0200

    shell correction

--- semantic_segmentation/DeepLabV3/dataloader.py
+++ semantic_segmentation/DeepLabV3/dataloader.py
@@ -41,5 +41,4 @@ if __name__ == "__main__":
 
     pass
 
-    training(['model_pre_full'],
-             path2=path2,val_loader=val_loader,train_loader=train_loader,train_dst=train_dst, val_dst=val_dst)
+    training(['model_pre_full'],path2=path2,val_loader=val_loader,train_loader=train_loader,train_dst=train_dst, val_dst=val_dst)
