0e0c162536316279fc491bd5f6fccb67ed6bb1b4
Mads C <S173934@student.dtu.dk>
2020-09-27 23:21:34 +0200

    hpc

--- training_hpc.sh
+++ training_hpc.sh
@@ -6,7 +6,7 @@
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "rusage[mem=128G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 24:00
+#BSUB -W 48:00
 #BSUB -u s173934@win.dtu.dk
 #BSUB -N
 # end of BSUB options
