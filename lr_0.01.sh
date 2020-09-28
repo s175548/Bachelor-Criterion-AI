6db08b4b01c2dcd396a85ef95312f60fe396b951
Mads C <S173934@student.dtu.dk>
2020-09-28 13:04:59 +0200

    lr, gpu = 1

--- lr_0.01.sh
+++ lr_0.01.sh
@@ -2,8 +2,8 @@
 #BSUB -J lr1
 #BSUB -o lr1%J.out
 #BSUB -q gpuv100
-#BSUB -n 1
-#BSUB -gpu "num=2:mode=exclusive_process"
+#BSUB -n 4
+#BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "rusage[mem=64G]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 24:00
