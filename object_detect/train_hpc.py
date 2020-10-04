34f414b6b346d5038e2819de225f58d20d7ce8f1
s175549 <s175549@student.dtu.dk>
2020-10-04 20:16:41 +0200

    Update train_hpc.py

--- object_detect/train_hpc.py
+++ object_detect/train_hpc.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
     print("Train set: %d, Val set: %d" %(len(train_dst), len(val_dst)))
 
     for lr in learning_rates:
-        model_name = 'resnet50'
+        model_name = 'mobilenet'
         model = define_model(num_classes=2,net=model_name)
         model.to(device)
 
