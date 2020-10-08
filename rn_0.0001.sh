ab114f1aff32c4a7b66ccc1e10a4d944d5428371
s175549 <s175549@student.dtu.dk>
2020-10-08 19:50:20 +0200

    sd

--- rn_0.0001.sh
+++ rn_0.0001.sh
@@ -5,7 +5,8 @@
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=128G]"
+#BSUB -R "rusage[mem=32G]"
+#BSUB -R "select[gpu32gb]"
 #BSUB -W 24:00
 #BSUB -u s175549@win.dtu.dk
 #BSUB -N
