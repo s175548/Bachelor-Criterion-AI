0d0e4cb5a77c1243a2fa2df3da6a18d00789b00f
Mads C <S173934@student.dtu.dk>
2020-10-14 13:05:05 +0200

    INIT EXP whole vs classifier v3

--- whole_Res.sh
+++ whole_Res.sh
@@ -15,5 +15,5 @@ echo "Running script..."
 cd ..
 source test-env/bin/activate
 cd Bachelor-Criterion-AI
-python3 semantic_segmentation/DeepLabV3/experiment_generator.py 0.01 DeepLab SGD False backbone_exp backbone/whole_network/ResNet
+python3 semantic_segmentation/DeepLabV3/experiment_generator.py 0.01 DeepLab SGD False backbone_exp backbone/whole_network/ResNet True
 echo "Done"
