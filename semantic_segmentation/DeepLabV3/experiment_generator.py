ff12543f789c3429d5120ee62bb2fe077a757226
Mads C <S173934@student.dtu.dk>
2020-10-14 11:20:49 +0200

    ..

--- semantic_segmentation/DeepLabV3/experiment_generator.py
+++ semantic_segmentation/DeepLabV3/experiment_generator.py
@@ -38,6 +38,8 @@ if __name__ == "__main__":
         exp_descrip = args['experiment description'][0]
         save_folder = args['folder name'][0]
         binary = args['binary setup'][0]
+        print("save folder: ",save_folder)
+        print("binary: ", binary)
         save_path = os.path.join(save_path, save_folder)
 
         if binary:
