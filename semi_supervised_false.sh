731728c35d0ad65faaf92be42994510c2ac84bba
Mads C <S173934@student.dtu.dk>
2020-11-01 13:05:59 +0100

    semi_super_hpc

--- semi_supervised_false.sh
+++ semi_supervised_false.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#BSUB -J versionSemiSupervised
-#BSUB -o versionSemiSupervised%J.out
+#BSUB -J versionSemiSupervised_false
+#BSUB -o versionSemiSupervised_false%J.out
 #BSUB -q gpuv100
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
@@ -15,5 +15,5 @@ echo "Running script..."
 cd ..
 source test-env/bin/activate
 cd Bachelor-Criterion-AI
-python3 semantic_segmentation/semi_supervised/main.py
+python3 semantic_segmentation/semi_supervised/main_setup_identical_to_supervised.py 0.01 DeepLab SGD True semi_setup_minus_semi semi_super/no_semi True False
 echo "Done"
---
+++ semi_supervised_true.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#BSUB -J versionSemiSupervised
+#BSUB -o versionSemiSupervised%J.out
+#BSUB -q gpuv100
+#BSUB -n 1
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=32G]"
+#BSUB -R "select[gpu32gb]"
+#BSUB -W 24:00
+#BSUB -u s173934@win.dtu.dk
+#BSUB -N
+# end of BSUB options
+
+echo "Running script..."
+cd ..
+source test-env/bin/activate
+cd Bachelor-Criterion-AI
+python3 semantic_segmentation/semi_supervised/main_setup_identical_to_supervised.py 0.01 DeepLab SGD True semi_setup semi_super/no_semi True True
+echo "Done"
