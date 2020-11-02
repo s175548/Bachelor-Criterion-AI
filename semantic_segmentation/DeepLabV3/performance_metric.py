dbc88f7584c0663199fc1be9281b68587c5ee0d1
s175548 <57710093+s175548@users.noreply.github.com>
2020-11-02 10:30:05 +0100

    ....

--- semantic_segmentation/DeepLabV3/performance_metric.py
+++ semantic_segmentation/DeepLabV3/performance_metric.py
@@ -6,6 +6,7 @@ from semantic_segmentation.DeepLabV3.utils.utils import Denormalize
 import torch
 from torchvision.models.segmentation import deeplabv3_resnet101
 import os
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 import PIL
 from semantic_segmentation.DeepLabV3.network.modeling import _segm_mobilenet
@@ -15,10 +16,11 @@ from semantic_segmentation.DeepLabV3.metrics import StreamSegMetrics
 from data_import.data_loader import convert_to_image
 
 
-def error_count(idx, pred_color,target_color, data_loader, labels,errors,false_positives,metric,reize=False):
-    pred=pred_color.copy()
-    target=target_color.copy()
-    if np.sum(target==1)!=0:
+def error_count(idx, pred_color, target_color, data_loader, labels, errors, false_positives, metric, reize=False,
+                size=None, scale=None):
+    pred = pred_color.copy()
+    target = target_color.copy()
+    if np.sum(target == 1) != 0:
         masks = data_loader.get_separate_segmentations(
             os.path.join(data_loader.data_path, data_loader.metadata_csv[idx, 3][1:]), labels=labels)
         buffer = 42
@@ -26,12 +28,12 @@ def error_count(idx, pred_color,target_color, data_loader, labels,errors,false_p
         ydim_s = []
         for mask in masks:
             label, mask = mask[0], np.squeeze(np.array(mask[1]).astype(np.uint8))
-            mask = F.center_crop(PIL.Image.fromarray(mask), output_size=256)
+            mask = F.center_crop(PIL.Image.fromarray(mask), output_size=size)
             mask = np.array(mask)
-            resize_shape = (int(mask.shape[0] * 0.33), int(mask.shape[1] * 0.33))
             if reize:
+                resize_shape = (int(mask.shape[0] * scale), int(mask.shape[1] * scale) * scale)
                 mask = F.resize(PIL.Image.fromarray(mask), resize_shape, PIL.Image.NEAREST)
-            mask=np.array(mask)
+            mask = np.array(mask)
             row, col = np.where(mask != 0)
             xdim = (np.maximum(np.min(row) - buffer, 0), np.minimum(np.max(row) + buffer, mask.shape[0]))
             xdim_s.append(xdim)
@@ -42,7 +44,7 @@ def error_count(idx, pred_color,target_color, data_loader, labels,errors,false_p
             if label == 'Insect bite':
                 errors[1, 0] += 1
                 errors[1, 1] += defect_found
-                metric[0].update(mask,pred[xdim[0]:xdim[1], ydim[0]:ydim[1]])
+                metric[0].update(mask, pred[xdim[0]:xdim[1], ydim[0]:ydim[1]])
             else:
                 errors[0, 0] += 1
                 errors[0, 1] += defect_found
@@ -51,33 +53,34 @@ def error_count(idx, pred_color,target_color, data_loader, labels,errors,false_p
             pred[xdim[0]:xdim[1], ydim[0]:ydim[1]] = 0
             target[xdim[0]:xdim[1], ydim[0]:ydim[1]] = 0
     else:
-        xdim_s=None
-        ydim_s=None
-    if np.sum(pred==1)>0:
-        false_positives+=1
-    target[target==2]=0
-    metric[2].update(target,pred)
-    target_color[target_color==2]=0
-    target_color,pred_color=color_target_pred(target_color, pred_color,pred, xdim_s, ydim_s)
-    return errors,false_positives,metric, target_color,pred_color
+        xdim_s = None
+        ydim_s = None
+    if np.sum(pred == 1) > 0:
+        false_positives += 1
+    target[target == 2] = 0
+    metric[2].update(target, pred)
+    target_color[target_color == 2] = 0
+    target_color, pred_color = color_target_pred(target_color, pred_color, pred, xdim_s, ydim_s)
+    return errors, false_positives, metric, target_color, pred_color
 
-def color_target_pred(target,pred,pred_false_pos,xdim_s,ydim_s):
-    target_tp=np.zeros(target.shape)
-    target_fp=np.zeros(target.shape)
-    fill=10
+
+def color_target_pred(target, pred, pred_false_pos, xdim_s, ydim_s):
+    target_tp = np.zeros(target.shape)
+    target_fp = np.zeros(target.shape)
+    fill = 3
     if xdim_s != None:
         for xdim, ydim in zip(xdim_s, ydim_s):
-            pred_crop=pred[xdim[0]:xdim[1], ydim[0]:ydim[1]]
-            pred[xdim[0]:xdim[1], ydim[0]:ydim[1]][pred_crop==1]=255
-            if np.sum(pred[xdim[0]:xdim[1], ydim[0]:ydim[1]] !=0)>0:
-                target_tp[xdim[0]:xdim[1],ydim[0]:ydim[0]+fill]=255
-                target[xdim[0]:xdim[1], ydim[0]:ydim[0] + fill]=0
+            pred_crop = pred[xdim[0]:xdim[1], ydim[0]:ydim[1]]
+            pred[xdim[0]:xdim[1], ydim[0]:ydim[1]][pred_crop == 1] = 255
+            if np.sum(pred[xdim[0]:xdim[1], ydim[0]:ydim[1]] != 0) > 0:
+                target_tp[xdim[0]:xdim[1], ydim[0]:ydim[0] + fill] = 255
+                target[xdim[0]:xdim[1], ydim[0]:ydim[0] + fill] = 0x
                 target_tp[xdim[0]:xdim[1], ydim[1] - fill:ydim[1]] = 255
                 target[xdim[0]:xdim[1], ydim[1] - fill:ydim[1]] = 0
-                target_tp[xdim[0]:xdim[0]+fill, ydim[0]:ydim[1]] = 255
-                target[xdim[0]:xdim[0]+fill, ydim[0]:ydim[1]] = 0
-                target_tp[xdim[1]-fill:xdim[1], ydim[0]:ydim[1]] = 255
-                target[xdim[1]-fill:xdim[1], ydim[0]:ydim[1]] = 0
+                target_tp[xdim[0]:xdim[0] + fill, ydim[0]:ydim[1]] = 255
+                target[xdim[0]:xdim[0] + fill, ydim[0]:ydim[1]] = 0
+                target_tp[xdim[1] - fill:xdim[1], ydim[0]:ydim[1]] = 255
+                target[xdim[1] - fill:xdim[1], ydim[0]:ydim[1]] = 0
             else:
                 target_fp[xdim[0]:xdim[1], ydim[0]:ydim[0] + fill] = 255
                 target[xdim[0]:xdim[1], ydim[0]:ydim[0] + fill] = 0
@@ -88,11 +91,10 @@ def color_target_pred(target,pred,pred_false_pos,xdim_s,ydim_s):
                 target_fp[xdim[1] - fill:xdim[1], ydim[0]:ydim[1]] = 255
                 target[xdim[1] - fill:xdim[1], ydim[0]:ydim[1]] = 0
 
-    pred_false_pos[pred_false_pos==1]=255
-    pred_rgb=np.dstack((pred_false_pos,pred,np.zeros(pred.shape)))
-    target_rgb=np.dstack((target_fp,target_tp,target*255))
-    return target_rgb,pred_rgb
-
+    pred_false_pos[pred_false_pos == 1] = 255
+    pred_rgb = np.dstack((pred_false_pos, pred, np.zeros(pred.shape)))
+    target_rgb = np.dstack((target_fp, target_tp, target * 255))
+    return target_rgb, pred_rgb
 
 
 """Arguments"""
@@ -100,13 +102,11 @@ def color_target_pred(target,pred,pred_false_pos,xdim_s,ydim_s):
 Villads = True
 model_name = 'DeepLab'
 n_classes = 1
-resize=False
-scale=0.5
+resize = False
+scale = 0.5
 binary = True
 device = torch.device('cpu')
 data_set = 'val'
-
-
 
 if Villads:
     path_original_data = r'/Users/villadsstokbro/Dokumenter/DTU/KID/5. Semester/Bachelor /leather_patches'
@@ -125,13 +125,13 @@ else:
 
 checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
 
-if model_name=='DeepLab':
-    model=deeplabv3_resnet101(pretrained=True, progress=True,num_classes=21, aux_loss=None)
-    model.classifier[-1] = torch.nn.Conv2d(256, n_classes+2, kernel_size=(1, 1), stride=(1, 1)).requires_grad_()
-    model.aux_classifier[-1] = torch.nn.Conv2d(256, n_classes+2, kernel_size=(1, 1), stride=(1, 1)).requires_grad_()
+if model_name == 'DeepLab':
+    model = deeplabv3_resnet101(pretrained=True, progress=True, num_classes=21, aux_loss=None)
+    model.classifier[-1] = torch.nn.Conv2d(256, n_classes + 2, kernel_size=(1, 1), stride=(1, 1)).requires_grad_()
+    model.aux_classifier[-1] = torch.nn.Conv2d(256, n_classes + 2, kernel_size=(1, 1), stride=(1, 1)).requires_grad_()
 else:
-    model=_segm_mobilenet('deeplabv3', 'mobile_net', output_stride=8, num_classes=n_classes+2,pretrained_backbone=True)
-
+    model = _segm_mobilenet('deeplabv3', 'mobile_net', output_stride=8, num_classes=n_classes + 2,
+                            pretrained_backbone=True)
 
 model.load_state_dict(checkpoint['model_state'])
 model.eval()
@@ -144,18 +144,16 @@ file_names_train = file_names_train[file_names_train != ".DS_S"]
 file_names_val = np.array([image_name[:-4] for image_name in os.listdir(path_val) if image_name[-5] != "k"])
 file_names_val = file_names_val[file_names_val != ".DS_S"]
 
+transform_function = et.ExtCompose([et.ExtCenterCrop(size=size),
+                                    et.ExtEnhanceContrast(),
+                                    et.ExtToTensor(),
+                                    et.ExtNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-transform_function = et.ExtCompose([et.ExtCenterCrop(size=256),
-                                        et.ExtEnhanceContrast(),
-                                        et.ExtToTensor(),
-                                        et.ExtNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-
-transform_function_resize = et.ExtCompose([et.ExtCenterCrop(size=1024),
-                                        et.ExtResize(scale=scale),
-                                        et.ExtEnhanceContrast(),
-                                        et.ExtToTensor(),
-                                        et.ExtNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-
+transform_function_resize = et.ExtCompose([et.ExtCenterCrop(size=size),
+                                           et.ExtResize(scale=scale),
+                                           et.ExtEnhanceContrast(),
+                                           et.ExtToTensor(),
+                                           et.ExtNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
 denorm = Denormalize(mean=[0.485, 0.456, 0.406],
                      std=[0.229, 0.224, 0.225])
@@ -168,7 +166,6 @@ else:
     color_dict = data_loader.color_dict
     target_dict = data_loader.get_target_dict(labels)
     annotations_dict = data_loader.annotations_dict
-
 
 train_dst = LeatherData(path_mask=path_train, path_img=path_train, list_of_filenames=file_names_train,
                         transform=transform_function, color_dict=color_dict, target_dict=target_dict)
@@ -184,10 +181,8 @@ if data_set == 'train':
 elif data_set == 'val':
     for i in range(len(val_dst)):
         train_images.append(val_dst.__getitem__(i))
-        if i==15:
+        if i == 15:
             break
-
-
 
 labels = ['02', 'Abassamento', 'Abbassamento', 'Area Punture insetti', 'Area aperta', 'Area vene', 'Buco', 'Cicatrice',
           'Cicatrice aperta', 'Contaminazione', 'Crease', 'Difetto di lavorazione', 'Dirt', 'Fianco', 'Fiore marcio',
@@ -195,8 +190,8 @@ labels = ['02', 'Abassamento', 'Abbassamento', 'Area Punture insetti', 'Area ape
           'Puntura insetto', 'Ruga', 'Rughe', 'Scopertura', 'Scratch', 'Smagliatura', 'Soffiatura', 'Struttura',
           'Taglio', 'Vena', 'Vene', 'Verruca', 'Wart', 'Zona aperta', 'verruca']
 
-metrics = [StreamSegMetrics(2),StreamSegMetrics(2),StreamSegMetrics(2)]
-false_positives=0
+metrics = [StreamSegMetrics(2), StreamSegMetrics(2), StreamSegMetrics(2)]
+false_positives = 0
 errors = np.array([[0, 0], [0, 0]])
 
 for i in range(len(train_images)):
@@ -211,18 +206,24 @@ for i in range(len(train_images)):
     pred = output.detach().max(dim=1)[1].cpu().squeeze().numpy()
     target = target.squeeze().numpy()
 
-    errors,false_positives,metric,target_color,pred_color=error_count(int(file_names_val[i]), pred.copy(),target.copy(), data_loader, labels,errors,false_positives,metrics,resize)
+    errors, false_positives, metric, target_color, pred_color = error_count(int(file_names_val[i]), pred.copy(),
+                                                                            target.copy(), data_loader, labels, errors,
+                                                                            false_positives, metrics, resize, size=size,
+                                                                            scale=scale)
 
     target = convert_to_image(target.squeeze(), color_dict, target_dict)
 
     pred = convert_to_image(pred.squeeze(), color_dict, target_dict)
     image = (denorm(train_images[i][0].detach().cpu().numpy()) * 255).transpose(1, 2, 0).astype(np.uint8)
     PIL.Image.fromarray(image.astype(np.uint8)).save(
-        os.path.join(save_path, r'binary', model_name, data_set+'1', r'{}_img.png'.format(file_names_val[i])), format='PNG')
+        os.path.join(save_path, r'binary', model_name, data_set + '1', r'{}_img.png'.format(file_names_val[i])),
+        format='PNG')
     PIL.Image.fromarray(pred_color.astype(np.uint8)).save(
-        os.path.join(save_path, r'binary', model_name, data_set+'1', r'{}_pred_color.png'.format(file_names_val[i])), format='PNG')
+        os.path.join(save_path, r'binary', model_name, data_set + '1', r'{}_pred_color.png'.format(file_names_val[i])),
+        format='PNG')
     PIL.Image.fromarray(target_color.astype(np.uint8)).save(
-        os.path.join(save_path, r'binary', model_name, data_set+'1', r'{}_mask_color.png'.format(file_names_val[i])), format='PNG')
+        os.path.join(save_path, r'binary', model_name, data_set + '1', r'{}_mask_color.png'.format(file_names_val[i])),
+        format='PNG')
 #    PIL.Image.fromarray(pred.astype(np.uint8)).save(
 #        os.path.join(save_path, r'binary', model_name, data_set + '1', r'{}_pred.png'.format(file_names_val[i])),
 #        format='PNG')
@@ -230,9 +231,11 @@ for i in range(len(train_images)):
 #        os.path.join(save_path, r'binary', model_name, data_set + '1', r'{}_mask.png'.format(file_names_val[i])),
 #        format='PNG')
 
-labels=['Insect bite','Binary','Good Area']
-new_list=[label+'\n'+'\n'.join([f"{name}, {performance}" for name, performance in metric[i].get_results().items()]) for i,label in enumerate(labels)]
-string='\n\n'.join(new_list)+f'\n\nBinary: {errors[0]} \nInsect Bite: {errors[1]} \nFalse positives: {false_positives}'
-f=open(os.path.join(save_path,'performance'),'w')
+labels = ['Insect bite', 'Binary', 'Good Area']
+new_list = [
+    label + '\n' + '\n'.join([f"{name}, {performance}" for name, performance in metric[i].get_results().items()]) for
+    i, label in enumerate(labels)]
+string = '\n\n'.join(
+    new_list) + f'\n\nBinary: {errors[0]} \nInsect Bite: {errors[1]} \nFalse positives: {false_positives}'
+f = open(os.path.join(save_path, 'performance'), 'w')
 f.write(string)
-
