7fc3bcc59468394023d9cf0578cc9aad40b31b49
s175548 <57710093+s175548@users.noreply.github.com>
2020-11-06 12:32:16 +0100

    ..

---
+++ prediction_good_skin.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#BSUB -J predictions_good_skin
+#BSUB -o predictions_good_skin%J.out
+#BSUB -q gpuv100
+#BSUB -n 1
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=32G]"
+#BSUB -R "select[gpu32gb]"
+#BSUB -W 24:00
+#BSUB -u s175548@win.dtu.dk
+#BSUB -N
+# end of BSUB options
+
+echo "Running script..."
+cd ..
+source test-env/bin/activate
+cd Bachelor-Criterion-AI
+python3 data_import/tif_evaluate.py
+echo "Done"
