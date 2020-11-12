482be20f031bbbfce5f3546b771489fd1640de7c
Madscba <57229906+Madscba@users.noreply.github.com>
2020-11-12 22:19:31 +0100

    semi_new loss

--- semantic_segmentation/semi_supervised/Training_win_semi.py
+++ semantic_segmentation/semi_supervised/Training_win_semi.py
@@ -24,7 +24,7 @@ num_classes=2
 output_stride=16
 save_val_results=False
 total_itrs=1000
-lr_g = 0.0002
+lr_g = 0.002
 lr_policy='step'
 step_size=1
 batch_size= 2# 16
@@ -35,7 +35,7 @@ random_seed=1
 val_interval= 55
 vis_num_samples= 2 #2
 enable_vis=True
-N_epochs= 150
+N_epochs= 50
 
 def save_ckpt(model,model_name=None,cur_itrs=None, optimizer=None,scheduler=None,best_score=None,save_path = os.getcwd(),lr=0.01,exp_description=''):
     """ save current model"""
@@ -134,7 +134,7 @@ def training(n_classes=3, model='DeepLab', load_models=False, model_path='/Users
         loss_unlabelled_d = []
         loss_fake_d = []
         loss_fake_g = []
-        gamma_one = gamma_two = .5 # Loss weights
+        gamma_one = gamma_two = .3 # Loss weights
 
         #Load model
         model_g = generator(1) #arg = number of gpu's
