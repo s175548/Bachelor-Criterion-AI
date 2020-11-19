cbf130823b9f177839bdfdb885e1c0da64ca3309
s175548 <57710093+s175548@users.noreply.github.com>
2020-11-19 21:44:26 +0100

    ...

--- tif_prediction_performance_metric.sh
+++ tif_prediction_performance_metric.sh
@@ -13,7 +13,7 @@
 
 echo "Running script..."
 cd ..
-source zhome/db/f/128823/Bachelor/test-env/bin/activate
+source /zhome/db/f/128823/Bachelor/test-env/bin/activate
 cd /zhome/db/f/128823/Bachelor/new_repo/Bachelor-Criterion-AI
 python3 semantic_segmentation/DeepLabV3/performance_metric_tif.py
 echo "Done"
