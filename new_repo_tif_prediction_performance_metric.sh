66255da9fd50e759cc703492436a8d22bacc919f
s175548 <57710093+s175548@users.noreply.github.com>
2020-11-20 10:36:37 +0100

    ....

--- new_repo_tif_prediction_performance_metric.sh
+++ new_repo_tif_prediction_performance_metric.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#BSUB -J predictions_tif
-#BSUB -o predictions_tif%J.out
+#BSUB -J new_repo_perform_tif
+#BSUB -o new_repo_perform_tif%J.out
 #BSUB -q gpuv100
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
@@ -14,6 +14,6 @@
 echo "Running script..."
 cd ..
 source /zhome/db/f/128823/Bachelor/test-env/bin/activate
-cd /zhome/db/f/128823/Bachelor/Bachelor-Criterion-AI
+cd /zhome/db/f/128823/Bachelor/new_repo/Bachelor-Criterion-AI
 python3 semantic_segmentation/DeepLabV3/performance_metric_tif.py
 echo "Done"
