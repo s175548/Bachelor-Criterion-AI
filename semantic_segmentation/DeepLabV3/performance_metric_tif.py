0d588676fad40133c464bd155e6c233f96d1c5c2
s175548 <57710093+s175548@users.noreply.github.com>
2020-11-30 14:08:11 +0100

    ...

--- semantic_segmentation/DeepLabV3/performance_metric_tif.py
+++ semantic_segmentation/DeepLabV3/performance_metric_tif.py
@@ -16,7 +16,7 @@ resize=True
 
 if villads:
     path='/Users/villadsstokbro/Dokumenter/DTU/KID/5. Semester/Bachelor /tif_images/annotations_RED_HALF02_grain_01_v.tif.json'
-    pred=PIL.Image.open('/Users/villadsstokbro/Dokumenter/DTU/KID/5. Semester/Bachelor /tif_images/RF_3C_orig_res.png')
+    pred=PIL.Image.open('/Users/villadsstokbro/Dokumenter/DTU/KID/5. Semester/Bachelor /tif_images/RF_3C_resize.png')
     target=PIL.Image.open('/Users/villadsstokbro/Dokumenter/DTU/KID/5. Semester/Bachelor /tif_images/RED_HALF02_grain_01_v_target_1d.png')
     save_path='/Users/villadsstokbro/Dokumenter/DTU/KID/5. Semester/Bachelor /tif_images/'
     path_meta_data=r'samples/model_comparison.csv'
@@ -26,7 +26,7 @@ elif HPC:
     path_meta_data = r'samples/model_comparison.csv'
     save_path = r'/work3/s173934/Bachelorprojekt/tif_img/RF_3C_resize_pred'
     path = r'/work3/s173934/Bachelorprojekt/tif_img/annotations_RED_HALF02_grain_01_v.tif.json'
-    pred=PIL.Image.open('/work3/s173934/Bachelorprojekt/tif_img/RF_3C_resize.png')
+    pred=PIL.Image.open('/work3/s173934/Bachelorprojekt/tif_img/RL_3C_resize.png')
     target=PIL.Image.open('/work3/s173934/Bachelorprojekt/tif_img/RED_HALF02_grain_01_v_target_1d.png')
     save_name='/RF_3C_resize'
 
