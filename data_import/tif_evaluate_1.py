cc5e731deb0351bbc0acc8fc87c74dd8c779c353
s175548 <57710093+s175548@users.noreply.github.com>
2020-11-30 13:12:07 +0100

    ...

--- data_import/tif_evaluate_1.py
+++ data_import/tif_evaluate_1.py
@@ -63,7 +63,7 @@ elif HPC:
 #'/RED_HALF02_grain_01_v.tif'
 
 data_loader = DataLoader(data_path=path_original_data, metadata_path=path_meta_data)
-image=load_tif_as_numpy_array(tif_path+'/WALKNAPPA_VDA_04_grain_01_v.tif')
+image=load_tif_as_numpy_array(tif_path+'/RED_HALF02_grain_01_v.tif')
 split_imgs, split_x_y,patch_dim = data_loader.generate_tif_patches(image, patch_size=patch_size,
                                                                          sliding_window=overlap)
 
@@ -135,5 +135,5 @@ for i in range(0,split_x_y[0],step_size):
     else:
         target_tif=np.vstack((target_tif,pred_stack))
 
-PIL.Image.fromarray(target_tif.astype(np.uint8)*255).save(tif_path+'/VDA_3C_resize.png')
+PIL.Image.fromarray(target_tif.astype(np.uint8)*255).save(tif_path+'/RL_3C_resize.png')
 
