b7eb532338e4f087684b343dca606abdfe1ee9ac
Mads C <S173934@student.dtu.dk>
2020-12-05 18:29:33 +0100

    sys append

--- semantic_segmentation/semi_supervised/Training_win_semi.py
+++ semantic_segmentation/semi_supervised/Training_win_semi.py
@@ -3,7 +3,7 @@ Made with inspiration from
 https://github.com/VainF/DeepLabV3Plus-Pytorch/blob/af50e37932732a2c06e331c54cc8c64820c307f4/main.py
 """
 import sys
-sys.path.append('/zhome/87/9/127623/BachelorProject/cropped_data/Bachelor-Criterion-AI')
+sys.path.append('/zhome/87/9/127623/BachelorProject/Bachelor-Criterion-AI')
 
 from tqdm import tqdm
 from PIL import ImageFile,Image
